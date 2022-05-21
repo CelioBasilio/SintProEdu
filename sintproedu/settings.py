6ee758816a8f41b56819ebf2cbdc6b94aa79e827
Celio <celiobasilio@hotmail.com>
2022-05-21 12:19:46 -0300

    Reparo em alguns problemas de css.

--- sintproedu/settings.py
+++ sintproedu/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -163,4 +165,4 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
 # Crispy forms
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
