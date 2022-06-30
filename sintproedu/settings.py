305b2ef11fc5e03b3384cb6ae8f3949e65978a99
Celio <celiobasilio@hotmail.com>
2022-06-30 17:29:48 -0300

    conf heroku

--- sintproedu/settings.py
+++ sintproedu/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u2y3dx6na!zy-4p!$u4%vs828_+w=y^&s$snz$a*(6+s*6$6sf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sintproedu.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -88,10 +89,7 @@ WSGI_APPLICATION = 'sintproedu.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
 
 
